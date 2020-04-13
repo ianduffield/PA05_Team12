adf3ebfafd88acfbfbb480cb16201a52598a61c0
WylliumHummh <51054277+WylliumHummh@users.noreply.github.com>
2020-04-13 10:52:21 -0400

    Update Athlete.java

--- src/lesson19/Athlete.java
+++ src/lesson19/Athlete.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Athlete extends Person{
 
 	Random random = new Random();
-	
+
 	// The Athlete is like the standard person, but has a chance to avoid infection, and may recover faster.
 	  int chance = random.nextInt(5);
 
@@ -16,7 +16,6 @@ public class Athlete extends Person{
 	  }
 	  public void tryToMove(){
 			super.tryToMoveRandomly();
-	    super.tryToMoveRandomly();
 		}
 
 	  public void setPosition(int x, int y){
@@ -26,14 +25,16 @@ public class Athlete extends Person{
 
 
 	  void tryToMoveRandomly(){
-	    int dx = random.nextInt(4)-1; // -1,0,1
-	    int dy = random.nextInt(4)-1; // -1,0,1
+			// The athlete can move for longer distances.
+	    int dx = random.nextInt(5)-2; // -2,-1,0,1,2
+	    int dy = random.nextInt(5)-2; // -2,-1,0,1,2
 	    if (isOK(super.x+dx, super.y+dy,super.country)) {
 	      super.moveTo(super.x+dx, super.y+dy);
 	    }
 	  }
 
 	  void checkForInfection(){
+			// Has a probability for whether the Athlete is infected.
 	    if (this.exposed && !this.infected) {
 	      this.chance = random.nextInt(5);
 	      if (this.chance > 1) {
@@ -43,6 +44,7 @@ public class Athlete extends Person{
 	    }
 	    this.age++;
 	    if (infected && !this.recovered && (this.age - this.infectionTime + this.chance > this.recoveryTime)) {
+			// The athlete will usually recover faster than others.
 	      this.recovered = true;
 	    }
 	  }
